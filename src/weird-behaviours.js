c4d3f06bd22cefe4c08139b9ea5645c006c4837d
Akash Barsagadey <akash_udaram.barsagadey@syngenta.com>
2020-06-15 18:06:02 +0530

    weired behaviors in js

---
+++ src/weird-behaviours.js
@@ -0,0 +1,21 @@
+[] == []; //false
+
+typeof NaN; //"number"
+
+true + true; //2
+
+typeof null; //"object"
+
+null instanceof Object; //false
+
++(+'ak') + 'ak'; //"NaNak"
+
+null != 0; //true
+
+null > 0; //false
+
+null >= 0; //true
+
+null <= 0; //true
+
+0.1 + 0.2 == 0.3; //false
